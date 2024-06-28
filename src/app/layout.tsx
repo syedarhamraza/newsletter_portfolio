a3465be8dbabca575886e51b0c7f4fce241ec28f
Syedarhamraza <arhamraza399@gmail.com>
2024-06-28 08:30:18 +0500

    seo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,57 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
-  icons: {
-    icon: "/logo.png",
-    shortcut: "/logo.png",
-    apple: "/apple-touch-icon.png",
+  
+  openGraph: {
+    title: "Arham Mastery",
+    description:
+      "Arham Mastery, as described, is a five-year-experienced web developer with proficiency in HTML, CSS, JavaScript, and React. They also have a solid understanding of C++. Arham is passionate about creating user-friendly web interfaces and staying updated with the latest trends in web development. They are ready to tackle diverse development challenges, reflecting a strong commitment to continuous learning and problem-solving in the field of web development.",
+    url: "https://www.arhammastery.dev/",
+    siteName: "Arham Mastery",
+    images: [
+      {
+        url: "https://www.arhammastery.dev/og.png",
+      },
+    ],
+    type: "website",
   },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+  },
+  icons: {
+    icon: "/android-chrome-512x512.png",
+    apple: "/apple-touch-icon",
+  },
+  manifest: "https://www.arhammastery.dev/site.webmanifest",
+  twitter: {
+    card: "summary_large_image",
+    title: "Arham Mastery",
+    description:
+      "Arham Mastery, as described, is a five-year-experienced web developer with proficiency in HTML, CSS, JavaScript, and React. They also have a solid understanding of C++. Arham is passionate about creating user-friendly web interfaces and staying updated with the latest trends in web development. They are ready to tackle diverse development challenges, reflecting a strong commitment to continuous learning and problem-solving in the field of web development.",
+    creator: "@arhammastery",
+    images: ["https://www.arhammastery.dev/og.png"],
+  },
+  generator: "Arham Mastery",
+  applicationName: "Arham Mastery",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "ArhamMastery",
+    "Arham Mastery",
+    "Arhammastery",
+    "arhammastery",
+    "Syed Arham Raza",
+    "Arham Raza",
+    "SyedArhamRaza",
+    "ArhamRaza",
+  ],
+  authors: [
+    { name: "Syed Arham Raza" },
+    { name: "ArhamMastery", url: "https://www.arhammastery.dev/" },
+  ],
+  creator: "Syed Arham Raza",
+  publisher: "Syed Arham Raza",
 }
 
 interface RootLayoutProps {
